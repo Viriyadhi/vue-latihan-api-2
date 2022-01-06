7dc19035d6cf934c16db852537303492356e5282
Viriyadhi <viriyadhitjan@gmail.com>
2022-01-07 00:21:52 +0700

    feat:[VS] new router and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/profession">Profession </router-link>
+      <router-link to="/hobby">Hobby</router-link>
+      <router-link to="/person">Person</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<script>
-export default {
-  name: "App",
+<style>
+* {
+  text-decoration: none;
+}
+#app {
+  font-size: 1.15rem;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+#nav {
+  display: flex;
+  padding: 2em;
+  justify-content: end;
+}
+
+#nav a {
+  font-weight: bolder;
+  color: #7e8a97;
+  padding-right: 2em;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Profession from "../views/Profession.vue";
+import Hobby from "../views/Hobby.vue";
+import Person from "../views/Person.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+const routes = [{
+        path: "/profession",
+        name: "Profession",
+        component: Profession,
+    },
+    {
+        path: "/hobby",
+        name: "Hobby",
+        component: Hobby,
+    },
+    {
+        path: "/person",
+        name: "person",
+        component: Person,
+    },
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 export default router;
